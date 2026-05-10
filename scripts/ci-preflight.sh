c275024416d6b0733503448ad7bd54e685b98192
Phuc Nguyen <trong552003@gmail.com>
2026-05-10 20:57:11 +0700

    ci: add reusable preflight script
    Signed-off-by: Phuc Nguyen <trong552003@gmail.com>

---
+++ scripts/ci-preflight.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+set -eu
+
+echo "[ci] preflight start"
+
+./scripts/build.sh
+./scripts/style-check.sh
+./scripts/test.sh
+./scripts/check-configs.sh
+
+echo "[ci] preflight complete"
